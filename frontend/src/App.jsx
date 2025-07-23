5188d1eb205a0b6fe9cb693bc8dc38fc933618a6
KhurshidSunny <khurshidsunny45@gmail.com>
2025-07-23 23:20:37 +0500

    Used Lazy Loading of components

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,14 +1,16 @@
-// src/App.jsx
+import { lazy, Suspense } from "react";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import { Toaster } from "react-hot-toast";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
-import AppLayout from "./components/AppLayout";
-import Home from "./pages/Home";
-import ProductList from "./pages/ProductList";
-import Checkout from "./pages/Checkout";
-import CreateProduct from "./pages/CreateProduct";
 import { CartProvider } from "./context/CartContext";
-import ProductDetails from "./pages/ProductDetails";
+import LoadingSpinner from "./components/LoadingSpinner";
+
+const AppLayout = lazy(() => import("./components/AppLayout"));
+const Home = lazy(() => import("./pages/Home"));
+const ProductList = lazy(() => import("./pages/ProductList"));
+const Checkout = lazy(() => import("./pages/Checkout"));
+const CreateProduct = lazy(() => import("./pages/CreateProduct"));
+const ProductDetails = lazy(() => import("./pages/ProductDetails"));
 
 // Create a QueryClient instance for React Query
 const queryClient = new QueryClient({
@@ -26,23 +28,24 @@ export default function App() {
       <CartProvider>
         <BrowserRouter>
           <div className="min-h-screen bg-gray-50">
-            <Routes>
-              <Route path="/" element={<AppLayout />}>
-                <Route index element={<Home />} />
-                <Route path="products" element={<ProductList />} />
-                <Route path="products/:id" element={<ProductDetails />} />
-                <Route path="create" element={<CreateProduct />} />
-                <Route path="checkout" element={<Checkout />} />
-              </Route>
-
-            </Routes>
+            <Suspense fallback={<LoadingSpinner />}>
+              <Routes>
+                <Route path="/" element={<AppLayout />}>
+                  <Route index element={<Home />} />
+                  <Route path="products" element={<ProductList />} />
+                  <Route path="products/:id" element={<ProductDetails />} />
+                  <Route path="create" element={<CreateProduct />} />
+                  <Route path="checkout" element={<Checkout />} />
+                </Route>
+              </Routes>
+            </Suspense>
             <Toaster
               position="top-right"
               toastOptions={{
                 duration: 3000,
                 style: {
-                  background: '#363636',
-                  color: '#fff',
+                  background: "#363636",
+                  color: "#fff",
                 },
               }}
             />
